c16c26d28cd2ea46762df1ccb2b097d2f20ad347
smjcrane <smjcrane@gmail.com>
2021-04-28 20:44:09 +0100

    Switch to hex coords

--- rainbow.py
+++ rainbow.py
@@ -1,5 +1,26 @@
 import drawSvg as draw
-import numpy as np
+from math import sqrt
+
+size = 10
+
+class Point:
+    def __init__(self, x, y, z):
+        if x+y+z != 0:
+            raise ValueError("Coords must some to 0")
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def rotate(self):
+        self.x, self.y, self.z = -self.z, -self.x, -self.y
+
+    def coords(self):
+        x = size * (     3/2 * self.x                  )
+        y = size * (sqrt(3)/2 * self.x  +  sqrt(3) * self.z)
+        return (x, y)
+
+    def __str__(self):
+        return "Point("+str(self.x)+", "+str(self.y)+","+str(self.z)+")"
 
 
 class RainbowSwirler:
@@ -10,15 +31,18 @@ class RainbowSwirler:
 
     def doInARainbow(self, pos, drawOne):
         v = pos
-        theta = np.radians(60)
-        rotate = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta), np.cos(theta)]])
         for i in range(6):
             drawOne(v, RainbowSwirler.rainbow[i])
-            v = rotate.dot(v)
+            v.rotate()
+            print(v)
 
-    def drawOneCircle(self, pos, color, radius):
+    def drawOneCircle(self, point, color, radius):
+        pos = point.coords()
         self.d.append(draw.Circle(pos[0], pos[1], radius,
             fill=color, stroke_width=2, stroke='black'))
+
+    def triangleOfCircles(self, pos, color):
+        pass
 
     def save(self):
         self.d.setPixelScale(2)  # Set number of pixels per geometry unit
@@ -26,5 +50,5 @@ class RainbowSwirler:
         self.d.savePng('rainbow.png')
 
 r = RainbowSwirler()
-r.doInARainbow([30, 0], (lambda v, c: r.drawOneCircle(v, c, 5)))
+r.doInARainbow(Point(3,0,-3), (lambda v, c: r.drawOneCircle(v, c, 5)))
 r.save()
